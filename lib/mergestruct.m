cd5c18de76c4c34dd93fd6e73ebe01c2b64cc233
Jonas Mueller <jonas@muellers.one>
2015-08-27 15:05:16 +0200

    Add ability to merge struct arrays

--- lib/mergestruct.m
+++ lib/mergestruct.m
@@ -1,7 +1,19 @@
 function outstruct = mergestruct(s1, s2)
 %MERGESTRUCT
 % Merge two given structs. If the same field exists in both S1 and S2,
-% the value from S2 is used.
+% the value from S2 is used. If either struct is a struct array, the two
+% are concatenated. For example:
+%
+% >> a.x = 1;
+% >> a.y = 2;
+% >> b.y = 5;
+% >> b.z = 10;
+% ans =
+%
+%     x: 1
+%     y: 5
+%     z: 10
+
 
 if not(isstruct(s1)) || not(isstruct(s2))
   error('Both arguments must be structs');
@@ -13,10 +25,19 @@ outstruct = s1;
 % Go through all fields of the 2nd struct and decide, what to do
 fnames = fieldnames(s2);
 
-for fidx = 1:length(fnames)
-  if isfield(outstruct, fnames{fidx}) && isstruct(outstruct.(fnames{fidx})) && isstruct(s2.(fnames{fidx}))
-    outstruct.(fnames{fidx}) = mergestruct(outstruct.(fnames{fidx}), s2.(fnames{fidx}));
-  else
-    outstruct.(fnames{fidx}) = s2.(fnames{fidx});
+if length(outstruct) > 1 || length(s2) > 1
+  for sidx = 1:length(s2)
+    for fidx = 1:length(fnames)
+      outstruct(length(outstruct)+sidx).(fnames{fidx}) = s2(sidx).(fnames{fidx});
+    end
+  end
+else
+  for fidx = 1:length(fnames)
+    if isfield(outstruct, fnames{fidx}) && isstruct(outstruct.(fnames{fidx})) && isstruct(s2.(fnames{fidx}))
+      outstruct.(fnames{fidx}) = mergestruct(outstruct.(fnames{fidx}), s2.(fnames{fidx}));
+    else
+      outstruct.(fnames{fidx}) = s2.(fnames{fidx});
+    end
   end
 end
+
--- spec/integration_spec.m
+++ spec/integration_spec.m
@@ -14,3 +14,17 @@ expect(c.x.z == 2);
 expect(strcmp(c.x.foo, 'hi there!'));
 expect(strcmp(c.bar, 'lalal'));
 expect(c.baz == 3);
+
+clear a b
+
+a(1).x = 1;
+a(2).x = 1;
+b(1).y = 2;
+
+c = mergestruct(a, b);
+
+expect(length(c) == 3);
+expect(c(1).x == 1);
+expect(c(2).x == 1);
+expect(c(3).x == []);
+expect(c(3).y == 2);
